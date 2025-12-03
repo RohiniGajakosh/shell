8591259be89dcc6e7705747abf1a5c54e28ea731
raj abhishek <rajfodu@gmail.com>
2025-12-03 22:48:25 +0530

    Update file1.sh

--- file1.sh
+++ file1.sh
@@ -1,54 +1,9 @@
-<<Comment 
-here i am creating a bash script to get the tool versions of the following tools:
-To get the latest release version for each tool installed on the system need to refer to their official release APIs:
-1. Terraform → Official HashiCorp Checkpoint API
-2. Docker → Official Docker GitHub release API
-3. AWS CLI → AWS GitHub release API
-4. GitLab Runner → GitLab Runner GitHub release API
-Comment
-
-# Ansible, docker , aws , gitlab, jenkins, terraform, kubernetes, helm, python, bash, linux
-
-##!/bin/bash
-# tools=(terraform docker aws gitlab-runner jenkins  ansible)
-# for tool in "${tools[@]}"; do
-#     if ! command -v $tool &> /dev/null; then
-#         echo "$tool could not be found"
-#     fi
-#     echo "Checking version for $tool"
-#     case $tool in
-#         terraform)
-#             terraform --version | head -n 1
-#             ;;
-#         docker)
-#             docker --version | awk '{print $1 " "  $3}' | sed 's/,//'
-#             ;;
-#         aws)
-#             aws --version | awk '{print $1 " "  $3}' | sed 's/,//'
-#             ;;
-#         gitlab-runner)
-#             gitlab-runner --version | head -n 1
-#             ;;
-#         jenkins)
-#             jenkins --version
-#             ;;   
-#         ansible)
-#             ansible --version | head -n 1
-#             ;;     
-#     esac
-#     echo "-----------------------------"
-# done
-
 #!/bin/bash
 installed_tools=("terraform" "docker" "aws" "gitlab-runner" "jenkins" "ansible")
-# latest_versions {
-#     "terraform": "https://checkpoint-api.hashicorp.com/v1/check/terraform",
-#     "docker": "https://api.github.com/repos/docker/docker-ce/releases/latest",
-#     "aws": "https://api.github.com/repos/aws/aws-cli/releases/latest",
-#     "gitlab-runner": "https://api.github.com/repos/gitlab-org/gitlab-runner/releases/latest",
-#     "jenkins": "https://api.github.com/repos/jenkinsci/jenkins/releases/latest",
-#     "ansible": "https://api.github.com/repos/ansible/ansible/releases/latest"
-# }
+EMAIL_TO="aaira8665@gmail.com"
+TEMPLATE_FILE="email_template.html"
+TEMP_EMAIL="/tmp/version_report.html"
+
 for tool in "${installed_tools[@]}"; do
     if command -v $tool &> /dev/null; then
         echo "$tool is installed."
@@ -79,13 +34,20 @@ for tool in "${installed_tools[@]}"; do
                 ;;
         esac
         echo "Current version of $tool: $current_version"
-        if [ "$current_version" != "$latest_version" ]; then
-            echo "A new version of $tool is available!" | mail -s "$tool Update Notification" aaira8665@gmail.com #to send notification if new version is available sending mail
-        fi
         echo "Latest version of $tool: $latest_version"
+        
+        if [ "$current_version" != "$latest_version" ]; then
+            sed -e "s/{{TOOL}}/$tool/g" \
+                -e "s/{{CURRENT_VERSION}}/$current_version/g" \
+                -e "s/{{LATEST_VERSION}}/$latest_version/g" \
+                -e "s/{{DATE}}/$(date '+%Y-%m-%d %H:%M:%S')/g" \
+                "$TEMPLATE_FILE" > "$TEMP_EMAIL"
+            
+            mail -s "$(echo -e "$tool Update Available\nContent-Type: text/html")" "$EMAIL_TO" < "$TEMP_EMAIL"
+            echo "Email sent for $tool update"
+        fi
         echo "-----------------------------"
     else
         echo "$tool is not installed."
     fi
 done
-
